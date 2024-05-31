f07ad9a5e03e96e2da90ad83adae5e461d280b86
ganyongz <1334723451@qq.com>
2024-05-31 09:34:31 +0800

    feat:'登录页布局及背景优化'

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,9 +2,9 @@
   <div class="login-container flx-center">
     <div class="login-box">
       <SwitchDark class="dark" />
-      <div class="login-left">
+      <!-- <div class="login-left">
         <img class="login-left-img" src="@/assets/images/login_left.png" alt="login" />
-      </div>
+      </div> -->
       <div class="login-form">
         <div class="login-logo">
           <!-- <img class="login-icon" src="@/assets/images/logo.svg" alt="" /> -->
