a92e21cd5de9b3a96b160c74ef7cf296144b8203
ganyongz <1334723451@qq.com>
2024-06-12 13:06:26 +0800

    feat:"欢迎页修改"

--- src/api/index.ts
+++ src/api/index.ts
@@ -72,12 +72,12 @@ class RequestHttp {
         if (data.code == ResultEnum.OVERDUE) {
           userStore.setToken("");
           router.replace(LOGIN_URL);
-          ElMessage.error(data.msg);
+          ElMessage.error(data.msg ? data.msg : data.mssage);
           return Promise.reject(data);
         }
         // 全局错误信息拦截（防止下载文件的时候返回数据流，没有 code 直接报错）
         if (data.code && data.code !== ResultEnum.SUCCESS) {
-          ElMessage.error(data.mssage);
+          ElMessage.error(data.mssage ? data.mssage : data.message);
           return Promise.reject(data);
         }
         // 成功请求（在页面上除非特殊情况，否则不用处理失败逻辑）
