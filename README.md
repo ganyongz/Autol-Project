b48b1a00c902069ca5d20dba67f4cfea5357b518
ganyongz <1334723451@qq.com>
2024-09-02 14:40:54 +0800

    feat:'windpower暂存'

--- README.md
+++ README.md
@@ -4,12 +4,6 @@
 
 Admin 一款基于 Vue3.4、TypeScript、Vite5、Pinia、Element-Plus 开源的后台管理框架，使用目前最新技术栈开发。项目提供强大的 [ProTable](https://juejin.cn/post/7166068828202336263) 组件，在一定程度上提高您的开发效率。另外本项目还封装了一些常用组件、Hooks、指令、动态路由、按钮级别权限控制等功能。
 
-### React 版本 🔥
-
-- 有需要请加底部微信了解、购买
-
-- Link：https://pro.spicyboy.cn
-
 ### 在线预览 👀
 
 - Link：https://admin.spicyboy.cn
---
+++ src/views/platform/windpower/components/alarmInfoVib.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-box" style="overflow: auto">
+    <dv-scroll-board :config="config" style="width: 98%; height: 280px" @mouseover="mouseoverHandler" @click="clickHandler" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref, toRefs } from "vue";
+let props = defineProps({
+  transmitDatas: {
+    type: Array,
+    default: () => [{ partName: "", alarmLevel: "" }]
+  }
+});
+let { transmitDatas } = toRefs(props);
+// 检查数组不为空，然后使用 .map() 方法提取 partName 和 alarmLevel
+const dataArray =
+  transmitDatas?.value.length > 0 ? transmitDatas?.value.map((item: any) => [item.partName, item.alarmLevel]) : [];
+const config = ref({
+  header: ["部件名称", "报警等级"],
+  data: [],
+  index: false,
+  // columnWidth: [50],
+  align: ["center"],
+  headerBGC: "#205f76"
+});
+config.value.data = dataArray as any;
+const mouseoverHandler = (e: any) => {
+  console.log(e);
+};
+
+const clickHandler = (e: any) => {
+  console.log(e);
+};
+</script>
